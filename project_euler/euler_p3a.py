9715720f7a340b6767f059182c473d27985cb5c2
meaot <meaot@users.noreply.github.com>
2014-08-25 23:59:48 -0500

    euler: solved problem 3

--- project_euler/euler_p3a.py
+++ project_euler/euler_p3a.py
@@ -1,25 +1,23 @@
-#
-# Project Euler
-# Problem 3
-#
-# The prime factors of 13195 are 5, 7, 13 and 29.
-# What is the largest prime factor of the number 600851475143 ?
-#
 from datetime import datetime
+from math import sqrt
+
 startTime = datetime.now()
 
 def isprime(n):
-    for i in range(2, n):
+    for i in xrange(2, n):
         if n%i == 0:
             return False
     return True
 
-number = (13195)
-primeFactors = []
-for j in range(2, number):
-    if number % j == 0 and isprime(j):
-        primeFactors.append(j)
-
-print primeFactors
+def greatest_prime_factor(m):
+	prime_factors = []
+	for i in xrange(2, int(sqrt(m))):
+		if m % i == 0 and isprime(i):
+			prime_factors.append(i)
+			m //= i
+	return prime_factors[-1]
+		
+print 'Greatest prime factor is: ',greatest_prime_factor(600851475143)
 
 print 'Execution time: ',(datetime.now()-startTime)
+			
